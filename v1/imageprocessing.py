1368449ec7dcaf1d2af9d003ee29b88956526b43
Danalache Sebastian <72085251+sebileague1@users.noreply.github.com>
2024-11-14 13:00:28 +0200

    Update imageprocessing.py

--- v1/imageprocessing.py
+++ v1/imageprocessing.py
@@ -3,29 +3,29 @@ import numpy as np
 from scipy import ndimage
 
 
-# Metoda care îmbunătățește contrastul imaginii
+# Metoda care îmbunătățește contrastul imaginii pentru o vizualizare mai clară
 def preprocess_retinal_image(image):
-    green_channel = image[:, :, 1]
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    contrast_enhanced = clahe.apply(green_channel)
-    denoised = cv2.medianBlur(contrast_enhanced, 5)
-    normalized = cv2.normalize(denoised, None, 0, 255, cv2.NORM_MINMAX)
-    return normalized
+    green_channel = image[:, :, 1]  # Extragerea canalului verde(Se folosește canalul verde pentru că oferă cel mai bun contrast pentru structurile retinale)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) #CLAHE îmbunătățește contrastul local al imaginii clipLimit=2.0: limitează amplificarea contrastului pentru a evita zgomotul excesiv
+    contrast_enhanced = clahe.apply(green_channel)  # si acum aplicam filtrul calalului verde
+    denoised = cv2.medianBlur(contrast_enhanced, 5) # reducerea zgomotului cu un filtru median de 5x5 pixeli(Înlocuiește fiecare pixel cu mediana valorilor din vecinătate)
+    normalized = cv2.normalize(denoised, None, 0, 255, cv2.NORM_MINMAX) # normalizam imaginea(Îmbunătățește vizibilitatea detaliilor în zonele întunecate și luminoase)
+    return normalized # si returnam imaginea normaliza cu filtrele aplicate pentru analiza ulterioara
 
 
 # Metoda pentru detectarea leziunilor întunecate (microanevrismele și hemoragiile)
 def detect_dark_lesions(preprocessed_image):
-    thresh = cv2.adaptiveThreshold(
+    thresh = cv2.adaptiveThreshold(   #Convertește imaginea în alb-negru folosind un prag adaptiv
         preprocessed_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY_INV, 11, 2
+        cv2.THRESH_BINARY_INV, 11, 2  # inversează rezultatul (leziunile devin albe)
     )
     kernel = np.ones((3, 3), np.uint8)
-    opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    filled = ndimage.binary_fill_holes(opened).astype(np.uint8) * 255
+    opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel) # Elimină zgomotul mic și detaliile nesemnificative
+    filled = ndimage.binary_fill_holes(opened).astype(np.uint8) * 255 #Umple orice goluri din interiorul regiunilor detectate
     num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(filled, connectivity=8)
     filtered_mask = np.zeros_like(filled)
     min_area, max_area = 5, 300
-    for label in range(1, num_labels):
+    for label in range(1, num_labels):  # Verifică dacă aria este între 5 și 300 pixeli si Păstrează doar componentele care se încadrează în acest interval
         area = stats[label, cv2.CC_STAT_AREA]
         if min_area <= area <= max_area:
             filtered_mask[labels == label] = 255
